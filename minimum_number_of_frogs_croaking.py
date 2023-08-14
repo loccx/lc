489fca3eafe06b45dda6dc1a54d3705135e94e58
loccx <kxingxu@gmail.com>
2023-08-14 16:57:32 -0700

    design browser history

---
+++ minimum_number_of_frogs_croaking.py
@@ -0,0 +1,22 @@
+'''
+You are given the string croakOfFrogs, which represents a combination of the string "croak" from different frogs, that is, multiple frogs can croak at the same time, so multiple "croak" are mixed.
+
+Return the minimum number of different frogs to finish all the croaks in the given string.
+
+A valid "croak" means a frog is printing five letters 'c', 'r', 'o', 'a', and 'k' sequentially. The frogs have to print all five letters to finish a croak. If the given string is not a combination of a valid "croak" return -1.
+'''
+
+class Solution:
+    def minNumberOfFrogs(self, croakOfFrogs: str) -> int:
+        count = {'c' : 0, 'r' : 0, 'o' : 0, 'a' : 0, 'k' : 0}
+
+        answer = 0
+
+        for char in croakOfFrogs:
+            if char not in count:
+                return -1
+            count[char] += 1
+            if count['c'] < count['r'] or count['r'] < count['o'] or count['o'] < count['u'] or count['u'] < count['n'] or count['n'] < count['t']:
+                return -1
+            answer = max(answer, count['c'] - count['t'])
+        return answer
---
+++ test.py
@@ -0,0 +1,2 @@
+test = {'a': 1, 'b': 2, 'c': 3}
+print(list(test.values()))
