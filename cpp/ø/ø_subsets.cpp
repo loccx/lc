ff4f0330daef6490d40405ad28cb3bbe05853c13
loccx <kxingxu@gmail.com>
2023-10-04 10:13:11 -0700

    subsets

---
+++ cpp/ø/ø_subsets.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+vector<vector<int>> subsets(vector<int> &input) {
+    vector<vector<int>> out;
+    vector<int> temp;
+
+    out.push_back({});
+    int outSize;
+    for (int k = 0; k < int(input.size()); k++) {
+        outSize = int(out.size());
+        for (int x = 0; x < outSize; x++) {
+            temp = out[x];
+            temp.push_back(input[k]);
+            out.push_back(temp);
+        }
+    }
+
+    return out;
+}
+
+int main() {
+    int n;
+    cout << "Size of input?\n";
+    cin >> n;
+
+    vector<int> inp(n);
+    for (int k = 0; k < n; k++) {
+        cin >> inp[k];
+    }
+
+    vector<vector <int>> result;
+
+    result = subsets(inp);
+
+    cout << "All possible subsets:\n\n";
+    for (auto l1: result) {
+        cout << "(";
+        for (int m = 0; m < int(l1.size()); m++) {
+            if (l1.size() == 1 || m == int(l1.size()) - 1) {
+                cout << l1[m];
+            }
+            else cout << l1[m] << ", ";
+        }
+        cout << ")\n";
+    }
+
+    return 0;
+}
