778e999680355636450c97adca74f781cbac9043
loccx <kxingxu@gmail.com>
2023-10-18 09:12:14 -0700

    swap done

--- cpp/swapping_nodes_in_a_linked_list.cpp
+++ cpp/swapping_nodes_in_a_linked_list.cpp
@@ -38,20 +38,19 @@ class Solution {
             }
 
             prev->next = two;
-            ListNode* temp = one->next;
-            one->next = two->next;
-            two->next = temp;
-            prev2->next = one;
-
-            while (dummy) {
-                cout << dummy->val << " ";
-                dummy = dummy->next;
+            if (one->next == two) {
+                one->next = two->next;
+                two->next = one;
             }
-            cout << '\n';
+            else {
+                ListNode* temp = two->next;
+                two->next = one->next;
+                one->next = temp;
+                prev2->next = one;
+            }
 
             return dummy->next;
         }
-
 };
 
 bool hasCycle(ListNode* head) {
@@ -60,31 +59,40 @@ bool hasCycle(ListNode* head) {
         if (s.end() != s.find(head)) {
             return true;
         }
+        cout << head->val << " ";
         s.insert(head);
         head=head->next;
     }
+    cout << '\n';
     return false;
 }
 
 void printList(ListNode* head) {
     if (!head) return;
-    while (head) {
-        cout << head->val << " ";
-        head = head->next;
+    ListNode* curr = head;
+    while (curr) {
+        cout << curr->val << " ";
+        curr = curr->next;
     }
     cout << '\n';
 }
 
 int main() {
-    ListNode* body5 = new ListNode(7);
-    ListNode* body4 = new ListNode(6, body5);
-    ListNode* body3 = new ListNode(4, body4);
-    ListNode* body2 = new ListNode(3, body3);
-    ListNode* body1 = new ListNode(2, body2);
-    ListNode* head = new ListNode(1, body1);
+    ListNode* body9 = new ListNode(5);
+    ListNode* body8 = new ListNode(9, body9);
+    ListNode* body7 = new ListNode(0, body8);
+    ListNode* body6 = new ListNode(3, body7);
+    ListNode* body5 = new ListNode(8, body6);
+    ListNode* body4 = new ListNode(7, body5);
+    ListNode* body3 = new ListNode(6, body4);
+    ListNode* body2 = new ListNode(6, body3);
+    ListNode* body1 = new ListNode(9, body2);
+    ListNode* head = new ListNode(7, body1);
+
+    // [7, 9, 6, 6, 7, 8, 3, 0, 9, 5]
 
     Solution test;
-    head = test.swapNodes(head, 2);
+    head = test.swapNodes(head, 5);
 
     if (!hasCycle(head)) printList(head);
     else cout << "has cycle!\n";
@@ -95,4 +103,6 @@ int main() {
     delete body3;
     delete body4;
     delete body5;
+
+    return 0;
 }
