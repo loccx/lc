fed1195f0cc0b74b8624c221f8b69b6637acc226
loccx <kxingxu@gmail.com>
2024-07-14 14:18:03 -0500

    subsets target yuh

--- cpp/k.cpp
+++ cpp/k.cpp
@@ -48,83 +48,35 @@ const static auto fast=[]{
     std::cout.tie(nullptr);
     return 0;
 }();
+
 //Solution class goes here
+
 class Solution {
 public:
-    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size() , cols = matrix[0].size();
-        
-        if(rows < 1){
-            return 0;
+    int waysToPartition(vector<int>& nums, int k) {
+        unordered_map<long long,int> l,r;
+        int n=nums.size();
+        long long s=accumulate(nums.begin(),nums.end(),0L);
+        long long left=0,right;
+        for(int x=0;x<n-1;x++){
+            left+=nums[x];
+            right=s-left;
+            r[right-left]++;
         }
 
-        printvec(matrix);
-        // calculate prefix sum for rows
-        for(int row = 0 ; row < rows ; row++){
-            for(int col = 1 ; col < cols ;col++){
-                matrix[row][col] =  matrix[row][col] + matrix[row][col -1];
-            }
+        int res=r[0];
+        left=0;
+        for(int x=0;x<n;x++){
+            left+=nums[x];
+            right=s-left;
+            int diff=k-nums[x];
+            res=max(res,l[-diff]+r[diff]);
+            l[right-left]++;
+            r[right-left]--;
         }
-        printvec(matrix);
-
-        int count = 0 , sum ;
-        cout<<"count: "<<count<<endl<<endl;
-        unordered_map<int , int> counter;
-        for(int colstart = 0 ; colstart < cols ;colstart++){
-            for(int col = colstart ; col < cols; col++){
-                cout<<"new column: "<<col<<endl;
-                cout<<"clearing dictionary...\n";
-                counter.clear();
-                counter[0] = 1;
-                cout<<"counter[0]: "<<counter[0]<<endl;
-                sum =0;
-                cout<<"clearing sum...\n";
-                for(int row = 0 ; row < rows ;row++){
-                    cout<<"k: "<<row<<endl;
-                    cout<<"x: "<<col<<endl;
-                    cout<<"matrix[row][col]: "<<matrix[row][col]<<endl;
-                    sum += matrix[row][col] - (colstart > 0 ? matrix[row][colstart - 1] : 0 );
-                    cout<<"sum: "<<sum<<endl;
-                    count += (counter.find(sum-target) != counter.end() ? counter[sum-target] : 0);
-                    cout<<"count updated: "<<count<<endl;
-                    cout<<"incrementing counter[sum]: \n";
-                    counter[sum]++;
-                    cout<<"counter[sum]: "<<counter[sum]<<endl;
-                    printmap(counter);
-                    cout<<endl;
-                }
-                cout<<endl;
-            }
-            cout<<endl;
-        }
-        return count;
-    }
-
-    void printvec(vvi& v){
-        cout<<"printed vec:\n";
-        cout<<"   ";
-        for(int k=0;k<v.size();k++)cout<<k<<' ';
-        cout<<endl;
-        for(int k=0;k<v.size();k++){
-            cout<<k<<"| ";
-            for(int x=0;x<v[0].size();x++){
-                cout<<v[k][x]<<' ';
-            }
-            cout<<endl;
-        }
-        cout<<endl;
-    }
-
-    void printmap(unordered_map<int,int>& mp){
-        cout<<'{';
-        for(auto p:mp){
-            cout<<p.first<<":"<<p.second<<' ';
-        }
-        cout<<"}\n";
+        return res;
     }
 };
-
-
 
 
 
@@ -132,11 +84,7 @@ public:
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
-    vvi inp={{10,10,10,10},
-             {10,1,1,10},
-             {10,1,1,10},
-             {10,10,10,10}};
     Solution s;
-    cout<< s.numSubmatrixSumTarget(inp,4) <<endl;
+    cout<< s <<endl;
     return 0;
 }
