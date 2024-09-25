9e2a2c3bddff3f680aaef76d1943572d967092fa
loccx <kxingxu@gmail.com>
2024-09-25 09:58:23 -0500

    daily

--- cpp/k.cpp
+++ cpp/k.cpp
@@ -41,10 +41,36 @@ const static auto fast=[]{
     return 0;
 }();
 //solution class
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        int n=arr1.size();
+        int m=arr2.size();
+        unordered_set<string> pref;
+        for(int& a:arr1){
+            string curr=to_string(a);
+            for(int k=1;k<=curr.size();k++){
+                pref.insert(curr.substr(0,k));
+            }
+        }
 
+        int res=0;
+        for(int& b:arr2){
+            string curr=to_string(b);
+            for(int k=curr.size();k>0;k--){
+                if(pref.count(curr.substr(0,k)))res=max(res,int(curr.substr(0,k).size()));
+            }
+        }
+        return res;
+    }
+};
  
  
  
 int main(){
+    vector<int> a1={1, 10, 100};
+    vector<int> a2={1000};
     Solution s;
+    cout<<s.longestCommonPrefix(a1,a2)<<endl;
+    return 0;
 }
