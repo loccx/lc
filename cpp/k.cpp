12bf53d25311fa749a71799932f7f3b0740d75ad
loccx <kxingxu@gmail.com>
2025-01-28 13:46:59 -0800

    max cycle in graph3;2u

--- cpp/k.cpp
+++ cpp/k.cpp
@@ -2,37 +2,29 @@
 using namespace std;
 #define endl '\n'
 #define fork(a,b) for(ll k=a;k<b;k++)
-#define fori(a,b) for(ll i=a;i<b;i++)
+#define forx(a,b) for(ll x=a;x<b;x++)
 #define mem(a,b) memset(a,b,sizeof(a))
-#define fill(a,b,c) fill(a,a+b,c)
+#define fill(a,b,v) fill(a,a+b,v)
+#define mp(a,b) make_pair(a,b)
 #define bug(x) cout<<#x<<" : "<<x<<'\n';
-#define hi cout<<"hi\n";
-#define inf 1<<28
 #define pb push_back
-#define PI M_PI
-#define PI2 M_PI_2
-#define c(k) int k;cin>>k;
-//#define f first
-//#define s second
+#define bg begin()
+#define ed end()
+#define all(x) x.bg,x.ed
+#define sz(x) int(x.size())
  
-typedef long long ll;
+using ll = long long;
 typedef pair<int,int> pi;
 typedef pair<double,double> pd;
 typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<double> vd;
-typedef vector<ll> vll;
-typedef vector<bool> vb;
-typedef vector<char> vc;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<double>> vvd;
-typedef vector<vector<ll>> vvll;
-typedef vector<vector<bool>> vvb;
-typedef vector<pi> vpi;
-typedef vector<pd> vpd;
-typedef vector<pll> vpll;
-typedef queue<int> qi;
-typedef queue<pair<int,int>> qpi;
+template<class t> using V=vector<t>;
+template<class t> using VV=V<V<t>>;
+template<class t> using Q=queue<t>;
+template<class t1,class t2> using MP=unordered_map<t1,t2>;
+typedef priority_queue<int> pqi;
+ 
+const ll M=1e9+7;
+const ll N=2*1e5+10;
 
 const static auto fast=[]{
     std::ios_base::sync_with_stdio(false);
@@ -41,21 +33,52 @@ const static auto fast=[]{
     return 0;
 }();
 //solution class
- class Solution {
+class Solution {
 public:
-    long long m=1e6+7;
-    vector<vector<int>> g(m,vector<int>(m));
-    void test(){
-        g[0][0]=1;
-        cout<<g[0][0]<<endl;
+    int longestCycle(vector<int>& edges) {
+        int n=edges.size();
+
+        vector<bool> vi(n,false);
+        vector<int> bt(n,-1);
+
+        int res=-1;
+        stack<int> s;
+
+        for(int k=0;k<n;k++){
+            if(!vi[k]&&edges[k]!=-1){
+                s.push(k);
+                while(!s.empty()){
+                    int c=s.top();
+                    s.pop();
+                    vi[c]=true;
+                    if(edges[c]!=-1){
+                        bt[edges[c]]=c;
+                        if(vi[edges[c]]){
+                            int o=edges[c];
+                            int cl=1;
+                            while(c!=o){
+                                c=bt[c];
+                                cl++;
+                            }
+                            if(cl!=-1)res=max(res,cl);
+                        }
+                        else{
+                            s.push(edges[c]);
+                        }
+                    }
+                }
+            }
+        }
+        return res;
     }
-    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        return 0;
-    }
-};
+}; 
+
+ 
  
  
 int main(){
+    V<int> inp={1,0};
+    V<int> inp2={-1,4,-1,2,0,4};
     Solution s;
-    s.test();
+    cout<<s.longestCycle(inp2)<<endl;
 }
