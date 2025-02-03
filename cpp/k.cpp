5c019ca733ac8cf6d8e026e1cbdb9f75f6c888aa
loccx <kxingxu@gmail.com>
2025-02-02 16:06:30 -0800

    longest consec sequence

--- cpp/k.cpp
+++ cpp/k.cpp
@@ -35,25 +35,31 @@ const static auto fast=[]{
 //solution class
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)return 0;
-        if(n==2)
-            if(nums[0]>nums[1])return 0;
-            else return 1;
-        if(nums[0]>nums[1])return 0;
-        if(nums[n-1]>nums[n-2])return n-1;
-        int l=1,r=n-2;
-        while(l<=r){
-            int m=l+(r-l)/2;
-            if(nums[m]>nums[m+1]&&nums[m]>nums[m-1])return m;
-            else if(nums[m]<nums[m-1])r=m-1;
-            else if(nums[m]<nums[m+1])l=m+1;
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int n:nums){
+            s.insert(n);
         }
-        return -1;
+        int res=0;
+        while(!s.empty()){
+            int cur=0;
+            int c=*s.begin();
+            int c1=c+1;
+            while(s.count(c)){
+                s.erase(c);
+                c--;
+                cur++;
+            }
+            while(s.count(c1)){
+                s.erase(c1);
+                c1++;
+                cur++;
+            }
+            res=max(res,cur);
+        }
+        return res;
     }
-};
- 
+}; 
  
  
 int main(){
