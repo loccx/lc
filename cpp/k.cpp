b1624847e29f7e7b19d3aeb6084621df76d8720b
loccx <kxingxu@gmail.com>
2025-02-07 15:23:16 -0800

    lockers aws

--- cpp/k.cpp
+++ cpp/k.cpp
@@ -1,73 +1,79 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-#define fork(a,b) for(ll k=a;k<b;k++)
-#define forx(a,b) for(ll x=a;x<b;x++)
-#define mem(a,b) memset(a,b,sizeof(a))
-#define fill(a,b,v) fill(a,a+b,v)
-#define mp(a,b) make_pair(a,b)
-#define bug(x) cout<<#x<<" : "<<x<<'\n';
-#define pb push_back
-#define bg begin()
-#define ed end()
-#define all(x) x.bg,x.ed
-#define sz(x) int(x.size())
- 
-using ll = long long;
-typedef pair<int,int> pi;
-typedef pair<double,double> pd;
-typedef pair<ll,ll> pll;
-template<class t> using V=vector<t>;
-template<class t> using VV=V<V<t>>;
-template<class t> using Q=queue<t>;
-template<class t1,class t2> using MP=unordered_map<t1,t2>;
-typedef priority_queue<int> pqi;
- 
-const ll M=1e9+7;
-const ll N=2*1e5+10;
 
-const static auto fast=[]{
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
-//solution class
-class Solution {
+enum Size {
+    S, M, L
+};
+
+struct Package {
+    Package() {};
+
+    long long id;
+    Size size;
+    bool frozen_food;
+};
+ 
+class LockerSystem {
+private:
+    const long long MAX_CAPACITY = 3;
+
+    unordered_map<Size,unordered_set<long long>> lockers;
+    queue<Package> locker_queue;
+    
+    unordered_map<Size,unordered_set<long long>> frozen_lockers;
+    queue<Package> frozen_locker_queue;
+
 public:
-    string reorganizeString(string s) {
-        vector<int> count(26, 0);
-        int maxind = 0;
-        for (char& let : s) {
-            if (++count[let - 'a'] > count[maxind]) {
-                maxind = let - 'a';
+    LockerSystem() {}
+
+    bool add_package(Package package) {
+        if (package.frozen_food) {
+            if (frozen_lockers[package.size].size() < MAX_CAPACITY) {
+                frozen_lockers[package.size].insert(package.id);
+                return true;
+            }
+            else {
+                frozen_locker_queue.push(package);
+                return false;
             }
         }
-
-        int it = 0;
-        if (2 * count[maxind] - 1 > s.size()) return "";
-
-        while (count[maxind]) {
-            s[it] = maxind + 'a';
-            count[maxind]--;
-            it += 2;
-        }
-
-        for (int k = 0; k < 26; k++) {
-            while (count[k]) {
-                if (it >= s.size()) it = 1;
-                s[it] = k + 'a';
-                count[k]--;
-                it += 2;
+        else {
+            if (lockers.size() < MAX_CAPACITY) {
+                lockers[package.size].insert(package.id);
+                return true;
+            }
+            else {
+                locker_queue.push(package);
+                return false;
             }
         }
-        return s;
+    }
+
+    bool retrieve_package(Package package) {
+        if (package.frozen_food) {
+            if (frozen_lockers.size() > ) {
+                frozen_lockers[package.size].insert(package.id);
+                return true;
+            }
+            else {
+                frozen_locker_queue.push(package);
+                return false;
+            }
+        }
+        else {
+            if (lockers.size() < MAX_CAPACITY) {
+                lockers[package.size].insert(package.id);
+                return true;
+            }
+            else {
+                locker_queue.push(package);
+                return false;
+            }
+        }
     }
 };
  
- 
 int main(){
-    string inp = "aba";
-    Solution s;
-    cout << s.reorganizeString(inp) << endl;
+    LockerSystem locker_system;
+    l.output_max();
 }
