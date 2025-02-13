363ac0c29a1c87e249b8ce5cad24580eaf7b755b
loccx <kxingxu@gmail.com>
2025-02-13 13:29:07 -0800

    multiply strings

--- cpp/k.cpp
+++ cpp/k.cpp
@@ -33,64 +33,75 @@ const static auto fast=[]{
     return 0;
 }();
 //solution class
-vector<vector<char>> falling(vector<vector<char>> grid) {
-    int m = grid.size();
-    int n = grid[0].size();
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        string res;
+        vector<string> nums;
+        if (num1 == "0" || num2 == "0") return "0";
+        if (num1.size() < num2.size())
+            swap(num1, num2);
 
-    int min_d = INT_MAX;
-    for (int c = 0; c < n; c++) {
-        int it = 0;
-        while (it < m) {
-            int d = 0;
-            while (it < m && grid[it][c] != 'F') {
-                it++;
-            }
-            it++;
-            while (it < m && grid[it][c] == '-') {
-                d++;
-                it++;
-            }
-            if (it < m && grid[it][c] == 'F') continue;
-            else if (it < m && grid[it][c] == '#' && d < min_d) {
-                min_d = d;
-            }
-            else if (it == m && d < min_d) {
-                min_d = d;
-            }
-        }
-    }
-    for (int c = 0; c < n; c++) {
-        for (int r = m-1; r >= 0; r--) {
-            if (grid[r][c] == 'F') {
-                grid[r + min_d][c] = 'F';
-                grid[r][c] = '-';
-            }
-        }
-    }
+        int n = num1.size();
+        int m = num2.size();
 
-    return grid;
-}
+        int bigsum = 0;
+        int mult = 0;
+        for (int x = m-1; x >= 0; x--) {
+            int carry = 0;
+            string sum = "";
+            for (int k = n-1; k >= 0; k--) {
+                int top = num1[k] - '0';
+                int bot = num2[x] - '0';
 
-void pg(vector<vector<char>> gr) {
-    for (auto& g : gr) {
-        for (auto& c : g) {
-            cout << c << ' ';
+                int prod = top * bot + carry;
+
+                if (prod > 9) carry = prod / 10;
+                else carry = 0;
+                char dig = (prod % 10) + '0';
+                sum.insert(0, 1, dig);
+            }
+            if (carry)
+                sum.insert(0, 1, (carry + '0'));
+            for (int m = 0; m < mult; m++) sum += '0';
+            mult++;
+            nums.push_back(sum);
         }
-        cout << endl;
+        int f = nums.size();
+        if (f == 1) return nums[0];
+
+        string curr = nums[0];
+
+        for (int k = 1; k < f; k++) {
+            if (curr.size() < nums[k].size()) swap(curr, nums[k]);
+            int carry = 0;
+            int ci = curr.size() - 1;
+            int ni = nums[k].size() - 1;
+            while (ni >= 0) {
+                int sum = (nums[k][ni] - '0') + (curr[ci] - '0') + carry;
+                carry = sum / 10;
+                curr[ci] = (sum % 10) + '0';
+                ni--;
+                ci--;
+            }
+            while (carry && ci >= 0) {
+                int sum = (curr[ci] - '0') + carry;
+                carry = sum / 10;
+                curr[ci] = (sum % 10) + '0';
+                ci--;
+            }
+            if (carry) curr.insert(0, 1, '1');
+        }
+
+        return curr;
     }
-    cout << endl;
-}
+};
  
-int main() {
-    vector<vector<char>> grid = {
-        {'F', 'F', 'F', 'F', 'F'},
-        {'-', '-', 'F', '-', '-'},
-        {'-', '-', 'F', '-', '-'},
-        {'-', 'F', 'F', '-', '-'},
-        {'-', '-', '-', '-', '-'},
-        {'-', '-', '-', '-', '-'},
-        {'-', '-', '-', '-', '-'}
-    };
-
-    pg(falling(grid));
+ 
+ 
+int main(){
+    string num1 = "123";
+    string num2 = "4567";
+    Solution s;
+    s.multiply(num1, num2);
 }
